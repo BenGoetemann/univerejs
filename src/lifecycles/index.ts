719ce557430ab1c95e11150845a6a2b8ab0d4134
BenGoetemann <b.goetemann@financesoft.de>
2025-01-14 20:20:01 +0100

    refactored naming of lifecycle folder to lifecycles, refactored exports

---
+++ src/lifecycles/index.ts
@@ -0,0 +1,25 @@
+export {
+    isSet,
+    gt,
+    lt,
+    eq,
+    neq,
+    notEmpty,
+    contains,
+    between,
+    evaluate,
+    and,
+    or,
+    startsWith,
+    endsWith,
+    matches,
+    isEmpty,
+    notContains,
+    gte,
+    lte
+  } from "./evaluations";
+  
+  export { set } from "./stateManipulations";
+  export { focusOn, chooseBetween } from "./promptInjections";
+  // ... anything else in "lifecycles"
+  
