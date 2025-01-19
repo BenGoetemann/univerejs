8d1078944e97506cbb79095e4c9a7f5a309894fa
BenGoetemann <b.goetemann@financesoft.de>
2025-01-19 02:58:19 +0100

    removed old type definitions

--- types/state.d.ts
+++
@@ -1,9 +0,0 @@
-import { ZodSchema } from "zod"; // Ensure zod is installed
-import { State } from "../src";
-
-declare global {
-    type TState = State
-
-}
-
-export { }; // This ensures it is treated as a module
